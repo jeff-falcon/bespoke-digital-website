a870a26467fb19d9f2f9607ada50f1d07368a636
Jeff Falcon <jeff@igloo.net>
2023-10-25 14:57:46 -0400

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		document.body.classList.add('isErrorPage');
+		return () => {
+			document.body.classList.remove('isErrorPage');
+		};
+	});
+</script>
+
+<section class="gutter">
+	<h1>{$page.status}</h1>
+	<h3>{$page.error?.message}</h3>
+</section>
+
+<style>
+	section {
+		padding-top: 5rem;
+		padding-bottom: 5rem;
+	}
+	:global(body.isErrorPage > div) {
+		height: 100vh;
+		display: flex !important;
+		flex-direction: column;
+	}
+	:global(body.isErrorPage main) {
+		flex: 1;
+		overflow: visible;
+	}
+	:global(body.isErrorPage #footer) {
+		margin-top: 0;
+	}
+	@media (min-height: 420px) {
+		section {
+			padding-top: 8rem;
+		}
+	}
+</style>
