c60aa44f2d9d547688ba6ab04f777634ccb5c800
Jeff Falcon <jeff@igloo.net>
2023-10-31 12:12:16 -0400

    make emails nicer to look at

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,58 +1,138 @@
-import * as nodemailer from 'nodemailer'
-import { GOOGLE_APP_EMAIL, GOOGLE_APP_PASSWORD } from '$env/static/private'
-import { addRow } from './google-sheets'
+import * as nodemailer from 'nodemailer';
+import { GOOGLE_APP_EMAIL, GOOGLE_APP_PASSWORD } from '$env/static/private';
+import { addRow } from './google-sheets';
 
-export function sendMail(email: string, name: string, message: string, fields?: { [key: string]: string }, subject?: string) {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: GOOGLE_APP_EMAIL,
-      pass: GOOGLE_APP_PASSWORD,
+function createHtmlBody(
+	name: string,
+	email: string,
+	message: string,
+	fields?: { [key: string]: string },
+	isExternal = false
+) {
+	let body = `<p style="margin: 0 0 10px"><img src="https://bespokedigital.com/bespoke-logo-black.png" width="183" height="32" alt="Bespoke"  /></p>
+  <h3 style="font-size: 16px; line-height: 1.5em; margin: 0 0 50px; font-weight: normal;">${
+		isExternal ? 'Receipt of your inquiry' : 'New website submission'
+	}</h3>
+  <h3 style="font-size: 24px; line-height: 1.5em; margin: 0; font-weight: normal;">From</h3>
+  <p style="font-size: 16px; line-height: 1.33em; margin: 8px 0 30px;">${name} - ${email}</p>
+  <h3 style="font-size: 24px; line-height: 1.5em; margin: 0; font-weight: normal;">Message</h3>
+  <p style="font-size: 16px; line-height: 1.33em; margin: 8px 0 30px; max-width: 600px;">${message}</p>`;
+
+	let text = `New form submission from:\n${name} <${email}>\n\nMessage:\n${message}\n`;
+
+	if (fields && Object.keys(fields).length > 0) {
+		body += `<br/><br/>
+      <h3 style="font-size: 24px; line-height: 1.5em; margin: 0; font-weight: normal;">Fields</h3>`;
+		body += Object.entries(fields)
+			.map(
+				([key, value]) =>
+					`<p style="font-size: 16px; line-height: 1.33em; margin: 8px 0 20px;"><b style="font-size: 14px;">${key}:</b><br/>
+          ${value}
+          </p>`
+			)
+			.join('');
+		text += '\n------\n\nFields:\n------\n';
+		text += Object.entries(fields)
+			.map(([key, value]) => `${key}: ${value}\n`)
+			.join('');
+	}
+
+	const html = `<html>
+  <head>
+  <style>
+    @font-face {
+      font-family: 'bespoke-digital-web';
+      src: url('https://bespokedigital.com/fonts/2466765/9b1d752a-575e-4d49-9cb7-db3223777f47.woff2') format('woff2'),
+        url('https://bespokedigital.com/fonts/2466765/b9eb9968-39c3-4d2d-882a-302cf860e114.woff') format('woff');
+      font-weight: bold;
+      font-style: normal;
     }
-  })
+    @font-face {
+      font-family: 'bespoke-digital-web';
+      src: url('https://bespokedigital.com/fonts/2466777/2792b279-5b1b-47b6-839f-5bc9163b442c.woff2') format('woff2'),
+        url('https://bespokedigital.com/fonts/2466777/ae29d4a4-5f5c-4a66-b4fe-55286a973389.woff') format('woff');
+      font-weight: normal;
+      font-style: normal;
+    }
+  </style>
+  </head>
+  <body style="font-family: bespoke-digital-web, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+  Cantarell, 'Open Sans', 'Helvetica Neue', Arial, sans-serif;">${body}</body>
+  </html>`;
 
-  let html = `<p><b>New form submission from:</b><br/><span style="font-size: 1.5em; font-weight: bold;">${name} &lt;${email}&gt;</span></p><br/><br/><h3 style="font-size: 24px; line-height: 1.5em; margin-bottom: 0;">Message:</h3><hr style="border: 0; height: 3px; background: #999" /><p>${message}</p>`
-  let text = `New form submission from:\n${name} <${email}>\n\nMessage:\n${message}\n`
+	return { html, text };
+}
 
-  if (fields && Object.keys(fields).length > 0) {
-    html += '<br/><br/><h3 style="font-size: 24px; line-height: 1.5em; margin-bottom: 0;">Fields:</h3><hr style="border: 0; height: 3px; background: #999" />'
-    html += Object.entries(fields).map(([key, value]) => `<p><b>${key}:</b><br/><span style="font-size: 1.25em">${value}</span></p>`).join('')
-    text += '\n------\n\nFields:\n------\n'
-    text += Object.entries(fields).map(([key, value]) => `${key}: ${value}\n`).join('')
-  }
+export function sendMail(
+	email: string,
+	name: string,
+	message: string,
+	fields?: { [key: string]: string },
+	subject?: string
+) {
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: GOOGLE_APP_EMAIL,
+			pass: GOOGLE_APP_PASSWORD
+		}
+	});
 
-  const promise1 = new Promise((resolve, reject) => {
-    transporter.sendMail({ from: `Bespoke Digital <${GOOGLE_APP_EMAIL}>`, to: `Bespoke Digital <${GOOGLE_APP_EMAIL}>`, replyTo: `${name} <${email}>`, subject: subject ?? 'Form Submission', html, text }, (err, info) => {
-      if (err) {
-        reject(err)
-        console.log({ err })
-      } else {
-        try {
-          addRow(email, name, message, fields)
-        } catch (err) {
-          console.log('failed to add row to google sheet', err)
-        }
-        resolve(info)
-        console.log({ info })
-      }
-    });
-  })
-  const promise2 = new Promise((resolve, reject) => {
-    transporter.sendMail({ from: `Bespoke Digital <${GOOGLE_APP_EMAIL}>`, to: `${name} <${email}>`, subject: 'Receipt: ' + (subject ?? 'Form Submission'), html, text }, (err, info) => {
-      if (err) {
-        reject(err)
-        console.log({ err })
-      } else {
-        try {
-          addRow(email, name, message, fields)
-        } catch (err) {
-          console.log('failed to add row to google sheet', err)
-        }
-        resolve(info)
-        console.log({ info })
-      }
-    });
-  })
+	const bespokeEmail = `Bespoke Digital <${GOOGLE_APP_EMAIL}>`;
 
-  return Promise.allSettled([promise1, promise2])
+	const promise1 = new Promise((resolve, reject) => {
+		const { html, text } = createHtmlBody(name, email, message, fields);
+		transporter.sendMail(
+			{
+				from: bespokeEmail,
+				to: bespokeEmail,
+				replyTo: `${name} <${email}>`,
+				subject: subject ?? 'Form Submission',
+				html,
+				text
+			},
+			(err, info) => {
+				if (err) {
+					reject(err);
+					console.log({ err });
+				} else {
+					try {
+						addRow(email, name, message, fields);
+					} catch (err) {
+						console.log('failed to add row to google sheet', err);
+					}
+					resolve(info);
+					console.log({ info });
+				}
+			}
+		);
+	});
+	const promise2 = new Promise((resolve, reject) => {
+		const { html, text } = createHtmlBody(name, email, message, fields, true);
+		transporter.sendMail(
+			{
+				from: bespokeEmail,
+				to: `${name} <${email}>`,
+				subject: 'Receipt: ' + (subject ?? 'Form Submission'),
+				html,
+				text
+			},
+			(err, info) => {
+				if (err) {
+					reject(err);
+					console.log({ err });
+				} else {
+					try {
+						addRow(email, name, message, fields);
+					} catch (err) {
+						console.log('failed to add row to google sheet', err);
+					}
+					resolve(info);
+					console.log({ info });
+				}
+			}
+		);
+	});
+
+	return Promise.allSettled([promise1, promise2]);
 }
