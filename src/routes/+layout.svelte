5669f65e8da60a3b205bd7e074363e847c58964e
Jeff Falcon <jeff@igloo.net>
2026-03-05 17:33:02 -0500

    register gsap

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,15 @@
 	import { store } from '$lib/store.svelte';
 	import Footer from '$lib/ui/nav/Footer.svelte';
 	import TopNav from '$lib/ui/nav/TopNav.svelte';
+	import { gsap } from 'gsap';
+	import { ScrollTrigger } from 'gsap/ScrollTrigger';
 	import { onMount } from 'svelte';
 	import 'video.js';
 	import 'video.js/dist/video-js.css';
 	import type { LayoutData } from './$types';
 	import './styles.css';
+
+	gsap.registerPlugin(ScrollTrigger);
 
 	interface Props {
 		data: LayoutData;
